9513126e025b4bd256ce260b8ca3f0371c06c77c
khirod96 <91793911+khirod96@users.noreply.github.com>
2021-11-23 16:34:23 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,10 @@
+class Demo3
+{
+	public static void main(String [] args)
+	{
+		StringBuilder i = new StringBuilder("APPLE");
+			System.out.println(i);
+		i.concat("BALL");
+			System.out.println(i);
+	}
+}
